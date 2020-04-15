17d6eb1c25ee4ef17fe7e66064b5bc9a50047434
ASHISH KUMAR <47477440+asfafasa@users.noreply.github.com>
2020-04-16 04:18:13 +0530

    2nd push

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,5 @@
-const mongoose = require('mongoose')
-
-const Uri = ("mongodb://Ashish:Ashish@mernbasic-shard-00-00-w0ajb.gcp.mongodb.net:27017,mernbasic-shard-00-01-w0ajb.gcp.mongodb.net:27017,mernbasic-shard-00-02-w0ajb.gcp.mongodb.net:27017/test?ssl=true&replicaSet=MERNBasic-shard-0&authSource=admin&retryWrites=true&w=majority")
-const db = async () => {
-    await mongoose.connect(Uri, {
-
-    });
-    console.log('db is connected')
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./prod');
+} else {
+    module.exports = require('./dev');
 }
-
-module.exports = Uri;
--- server.js
+++ server.js
@@ -1,26 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
 
 const items = require('./routes/api/items');
+const users = require('./routes/api/users');
 
 const app = express();
 
 //Body Parser Miodddle Ware
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+// Cookie Parser Middle Ware
+app.use(cookieParser())
 
 // DB Config
-const Uri = require('./config/keys')
+const config = require('./config/keys')
 
-mongoose.connect(Uri, {
+mongoose.connect(config.mongoURI, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
 }).then(() => console.log("Mongo Db connected")).catch(err => console.log(err))
 
 // Connect to mongo
 
+
 //  Use Routes
 app.use('/api/items', items);
+app.use('/api/users', users);
 
 const port = process.env.PORT || 5000;
 
